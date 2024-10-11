0ff1ad34abad0e2e6b5c1fedf98e842c30f7e3fa
Ilinca Vultur <ilinca.vultur@hagleitner.com>
2024-10-11 11:41:00 +0200

    Change groupId

--- speckandroidlibrary/build.gradle.kts
+++ speckandroidlibrary/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 publishing {
     publications {
         register<MavenPublication>("release") {
-            groupId = "hsm.digital.android"
+            groupId = "com.github.hsm.digital.android"
             artifactId = "speck4896cipher"
             version = "1.0.0"
 
